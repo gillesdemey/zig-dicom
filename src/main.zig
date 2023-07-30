6fdc093e659fd3126b3d7c45073602874177c170
Gilles De Mey <gilles.de.mey@gmail.com>
2023-07-30 23:27:52 +0200

    use more fns

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,9 @@
 const std = @import("std");
 const fmt = std.fmt;
 const mem = std.mem;
+const io = std.io;
 
+const File = std.fs.File;
 const Endian = std.builtin.Endian;
 const Case = fmt.Case;
 
@@ -12,6 +14,9 @@ const MAGIC_WORD = "DICM";
 const DICOMError = error{
     NoMagicWord,
 };
+
+const Element = struct { tag: Tag, vr: [2]u8, vl: u16, value: u32 };
+const Tag = struct { group: [2]u8, element: [2]u8 };
 
 pub fn main() !void {
     std.debug.print("⚡︎ Zig DICOM v{s}\n\n", .{"0.1.0"});
@@ -28,42 +33,60 @@ pub fn main() !void {
     std.debug.print("file\t\t{s}\n", .{path});
     std.debug.print("size\t\t{s}\n", .{fmt.fmtIntSizeDec(file_size)});
 
-    // 2. skip 128 bytes, this is the preamble
-    try file.seekTo(128);
-
-    // 3. check for the DICM magic string
-    const magic_word = try file_reader.readBytesNoEof(4);
-    if (!mem.eql(u8, &magic_word, MAGIC_WORD)) {
-        return DICOMError.NoMagicWord;
-    }
+    try verifyPreamble(file_reader);
 
     // now we must read a tag + value (collectively called an element)
 
     // Must read metadata as LittleEndian explicit VR
     // Read the length of the metadata elements: (0002,0000) MetaElementGroupLength
-    const group = try file_reader.readBytesNoEof(2);
-    const element = try file_reader.readBytesNoEof(2);
-
-    // Explicit Transfer Syntax, read 2 byte VR:
-    const vr = try file_reader.readBytesNoEof(2);
-    // Now read the VL based on the VR (UL) (16-bit unsigned integer)
-    const vl = try file_reader.readIntLittle(u16);
-    // now read the value based on the VR and VL
-    const value = try file_reader.readIntLittle(u32);
+    const element = try readElement(file_reader);
+    const tag = element.tag;
 
     std.debug.print("\n", .{});
     std.debug.print("╔═══════ PREAMBLE ══════╗\n", .{});
     std.debug.print("║  0x00(×128)         \t║\n", .{});
-    std.debug.print("║  MAGIC_WORD   {s}   \t║\n", .{&magic_word});
+    std.debug.print("║  MAGIC_WORD   ✔️     \t║\n", .{});
     std.debug.print("╠═══════ ELEMENT ═══════╣\n", .{});
     std.debug.print("║                       ║\n", .{});
     std.debug.print("╟───────── TAG ─────────╢\n", .{});
-    std.debug.print("║ group         {s}   \t║\n", .{fmt.fmtSliceHexLower(&group)});
-    std.debug.print("║ element       {s}   \t║\n", .{fmt.fmtSliceHexLower(&element)});
+    std.debug.print("║ group         {s}   \t║\n", .{fmt.fmtSliceHexLower(&tag.group)});
+    std.debug.print("║ element       {s}   \t║\n", .{fmt.fmtSliceHexLower(&tag.element)});
     std.debug.print("╟─────── VR / VL ───────╢\n", .{});
-    std.debug.print("║ VR            {s}   \t║\n", .{&vr});
-    std.debug.print("║ VL            {d}   \t║\n", .{vl});
+    std.debug.print("║ VR            {s}   \t║\n", .{&element.vr});
+    std.debug.print("║ VL            {d}   \t║\n", .{element.vl});
     std.debug.print("╟──────── VALUE ────────╢\n", .{});
-    std.debug.print("║ value         {any} \t║\n", .{value});
+    std.debug.print("║ value         {any} \t║\n", .{element.value});
     std.debug.print("╚═══════════════════════╝\n", .{});
+}
+
+// check the preamble
+// (first 132 bytes, 128 empty bytes + 4 byte magic word, DICM)
+fn verifyPreamble(reader: File.Reader) !void {
+    try reader.skipBytes(128, .{}); // skip 128 bytes
+
+    // check for the DICM magic string
+    const magic_word = try reader.readBytesNoEof(4);
+    if (!mem.eql(u8, &magic_word, MAGIC_WORD)) {
+        return DICOMError.NoMagicWord;
+    }
+}
+
+fn readElement(reader: File.Reader) !Element {
+    const tag = try readTag(reader);
+
+    // Explicit Transfer Syntax, read 2 byte VR:
+    const vr = try reader.readBytesNoEof(2);
+    // Now read the VL based on the VR (UL) (16-bit unsigned integer)
+    const vl = try reader.readIntLittle(u16);
+    // now read the value based on the VR and VL
+    const value = try reader.readIntLittle(u32);
+
+    return Element{ .tag = tag, .vr = vr, .vl = vl, .value = value };
+}
+
+fn readTag(reader: File.Reader) !Tag {
+    const group = try reader.readBytesNoEof(2);
+    const element = try reader.readBytesNoEof(2);
+
+    return Tag{ .group = group, .element = element };
 }
